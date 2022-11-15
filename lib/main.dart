b717e30ff13f68ef7223aeec3dfa746ad0dd60d4
chirag <chiragthummar16@gmail.com>
2022-11-15 11:11:47 +0530

    Basic Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import './screens/get_started.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Weather App',
+      home: GetStarted(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
